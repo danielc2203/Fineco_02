a92034c5600179bf0f8056fbf8122c4f5cf3c4bb
Daniel Castro R <danielc2203@gmail.com>
2022-10-17 13:05:45 -0500

    Archivo Config

--- global/conexion.php
+++ global/conexion.php
@@ -1,8 +1,10 @@
 <?php
-$servidor="mysql:dbname=finecoApp;host=localhost";
+include('config.php');
+
+$servidor="mysql:dbname=".BD.";host=".SERVIDOR;
 
 try {
-    $pdo= new PDO($servidor,"root","Admin2admin", array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+    $pdo= new PDO($servidor,USUARIO,PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
 
     echo "<scritp> alert('Conectado...') </script>;";
 
---
+++ global/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("SERVIDOR", "localhost");
+define("USUARIO", "root");
+define("PASSWORD", "Admin2admin");
+define("BD", "finecoApp");
+
+
+?>
