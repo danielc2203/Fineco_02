38d8d07ba90ce072373689882a0ec82331476f32
Daniel Castro R <danielc2203@gmail.com>
2022-10-17 19:00:35 -0500

    Crud Clientes

--- modulos/clientes.php
+++ modulos/clientes.php
@@ -1,3 +1,11 @@
 <?php
-include('../global/sesiones.php')
+include('../global/sesiones.php');
+include('../global/conexion.php');
+
+$sentenciaSQL=$pdo->prepare("SELECT * FROM clientes ");
+$sentenciaSQL->execute();
+$finecoclientes = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+
+//$finecoclientes=$registro['finecoclientes'];
+
 ?>
---
+++ template/VistaClientes2.php
@@ -0,0 +1,544 @@
+<?php include ('../modulos/clientes.php') ?>
+
+<!-- HEADER - UBICADO EN RECURSOS -->
+
+<?php include ('../recursos/header.php') ?>
+<!-- FIN DE HEADER -->
+
+  <!-- DataTables -->
+  <link rel="stylesheet" href="../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+<!-- Main Sidebar Container -->
+
+<?php include ('../recursos/sidebar.php') ?>
+
+<!-- Fin de Sidebar -->
+
+
+  <!-- Contenido de clientes -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Listado de Clientes</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+              <li class="breadcrumb-item active">Clientes</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Tabla de clientes FINECO APP</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Nombres</th>
+                    <th>Apellidos</th>
+                    <th>Documento</th>
+                    <th>Estado</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>ok</td>
+                    <td>Daniel</td>
+                    <td>Castro</td>
+                    <td>1033222111</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>Fineco</td>
+                    <td>Internet
+                      Explorer 5.0
+                    </td>
+                    <td>Win 95+</td>
+                    <td>5</td>
+                    <td>Inactivo</td>
+                  </tr>
+                  <tr>
+                    <td>Fineco</td>
+                    <td>Internet
+                      Explorer 5.5
+                    </td>
+                    <td>Win 95+</td>
+                    <td>5.5</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>Fineco</td>
+                    <td>Internet
+                      Explorer 6
+                    </td>
+                    <td>Win 98+</td>
+                    <td>6</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>Fineco</td>
+                    <td>Internet Explorer 7</td>
+                    <td>Win XP SP2+</td>
+                    <td>7</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>Fineco</td>
+                    <td>AOL browser (AOL desktop)</td>
+                    <td>Win XP</td>
+                    <td>6</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Firefox 1.0</td>
+                    <td>Win 98+ / OSX.2+</td>
+                    <td>1.7</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Firefox 1.5</td>
+                    <td>Win 98+ / OSX.2+</td>
+                    <td>1.8</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Firefox 2.0</td>
+                    <td>Win 98+ / OSX.2+</td>
+                    <td>1.8</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Firefox 3.0</td>
+                    <td>Win 2k+ / OSX.3+</td>
+                    <td>1.9</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Camino 1.0</td>
+                    <td>OSX.2+</td>
+                    <td>1.8</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Camino 1.5</td>
+                    <td>OSX.3+</td>
+                    <td>1.8</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Netscape 7.2</td>
+                    <td>Win 95+ / Mac OS 8.6-9.2</td>
+                    <td>1.7</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Netscape Browser 8</td>
+                    <td>Win 98SE+</td>
+                    <td>1.7</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Netscape Navigator 9</td>
+                    <td>Win 98+ / OSX.2+</td>
+                    <td>1.8</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Mozilla 1.0</td>
+                    <td>Win 95+ / OSX.1+</td>
+                    <td>1</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Mozilla 1.1</td>
+                    <td>Win 95+ / OSX.1+</td>
+                    <td>1.1</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Mozilla 1.2</td>
+                    <td>Win 95+ / OSX.1+</td>
+                    <td>1.2</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Mozilla 1.3</td>
+                    <td>Win 95+ / OSX.1+</td>
+                    <td>1.3</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Mozilla 1.4</td>
+                    <td>Win 95+ / OSX.1+</td>
+                    <td>1.4</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Mozilla 1.5</td>
+                    <td>Win 95+ / OSX.1+</td>
+                    <td>1.5</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Mozilla 1.6</td>
+                    <td>Win 95+ / OSX.1+</td>
+                    <td>1.6</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Mozilla 1.7</td>
+                    <td>Win 98+ / OSX.1+</td>
+                    <td>1.7</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Mozilla 1.8</td>
+                    <td>Win 98+ / OSX.1+</td>
+                    <td>1.8</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Seamonkey 1.1</td>
+                    <td>Win 98+ / OSX.2+</td>
+                    <td>1.8</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>ARMADA NACIONAL</td>
+                    <td>Epiphany 2.20</td>
+                    <td>Gnome</td>
+                    <td>1.8</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>CREMIL</td>
+                    <td>Safari 1.2</td>
+                    <td>OSX.3</td>
+                    <td>125.5</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>CREMIL</td>
+                    <td>Safari 1.3</td>
+                    <td>OSX.3</td>
+                    <td>312.8</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>CREMIL</td>
+                    <td>Safari 2.0</td>
+                    <td>OSX.4+</td>
+                    <td>419.3</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>CREMIL</td>
+                    <td>Safari 3.0</td>
+                    <td>OSX.4+</td>
+                    <td>522.1</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>CREMIL</td>
+                    <td>OmniWeb 5.5</td>
+                    <td>OSX.4+</td>
+                    <td>420</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>CREMIL</td>
+                    <td>iPod Touch / iPhone</td>
+                    <td>iPod</td>
+                    <td>420.1</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>CREMIL</td>
+                    <td>S60</td>
+                    <td>S60</td>
+                    <td>413</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Opera 7.0</td>
+                    <td>Win 95+ / OSX.1+</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Opera 7.5</td>
+                    <td>Win 95+ / OSX.2+</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Opera 8.0</td>
+                    <td>Win 95+ / OSX.2+</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Opera 8.5</td>
+                    <td>Win 95+ / OSX.2+</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Opera 9.0</td>
+                    <td>Win 95+ / OSX.3+</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Opera 9.2</td>
+                    <td>Win 88+ / OSX.3+</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Opera 9.5</td>
+                    <td>Win 88+ / OSX.3+</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Opera for Wii</td>
+                    <td>Wii</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Nokia N800</td>
+                    <td>N800</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOMAG</td>
+                    <td>Nintendo DS browser</td>
+                    <td>Nintendo DS</td>
+                    <td>8.5</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>CASUR</td>
+                    <td>Konqureror 3.1</td>
+                    <td>KDE 3.1</td>
+                    <td>3.1</td>
+                    <td>Inactivo</td>
+                  </tr>
+                  <tr>
+                    <td>CASUR</td>
+                    <td>Konqureror 3.3</td>
+                    <td>KDE 3.3</td>
+                    <td>3.3</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>CASUR</td>
+                    <td>Konqureror 3.5</td>
+                    <td>KDE 3.5</td>
+                    <td>3.5</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>FOPEP</td>
+                    <td>Internet Explorer 4.5</td>
+                    <td>Mac OS 8-9</td>
+                    <td>-</td>
+                    <td>Reirado</td>
+                  </tr>
+                  <tr>
+                    <td>FOPEP</td>
+                    <td>Internet Explorer 5.1</td>
+                    <td>Mac OS 7.6-9</td>
+                    <td>1</td>
+                    <td>Inactivo</td>
+                  </tr>
+                  <tr>
+                    <td>FOPEP</td>
+                    <td>Internet Explorer 5.2</td>
+                    <td>Mac OS 8-X</td>
+                    <td>1</td>
+                    <td>Inactivo</td>
+                  </tr>
+                  <tr>
+                    <td>EJERCITO</td>
+                    <td>NetFront 3.1</td>
+                    <td>Embedded devices</td>
+                    <td>-</td>
+                    <td>Inactivo</td>
+                  </tr>
+                  <tr>
+                    <td>EJERCITO</td>
+                    <td>NetFront 3.4</td>
+                    <td>Embedded devices</td>
+                    <td>-</td>
+                    <td>Activo</td>
+                  </tr>
+                  <tr>
+                    <td>EJERCITO</td>
+                    <td>Dillo 0.8</td>
+                    <td>Embedded devices</td>
+                    <td>-</td>
+                    <td>Reirado</td>
+                  </tr>
+                  <tr>
+                    <td>EJERCITO</td>
+                    <td>Links</td>
+                    <td>Text only</td>
+                    <td>-</td>
+                    <td>Reirado</td>
+                  </tr>
+                  <tr>
+                    <td>EJERCITO</td>
+                    <td>Lynx</td>
+                    <td>Text only</td>
+                    <td>-</td>
+                    <td>Reirado</td>
+                  </tr>
+                  <tr>
+                    <td>EJERCITO</td>
+                    <td>IE Mobile</td>
+                    <td>Windows Mobile 6</td>
+                    <td>-</td>
+                    <td>Inactivo</td>
+                  </tr>
+                  <tr>
+                    <td>EJERCITO</td>
+                    <td>PSP browser</td>
+                    <td>PSP</td>
+                    <td>-</td>
+                    <td>Inactivo</td>
+                  </tr>
+                  <tr>
+                    <td>Other browsers</td>
+                    <td>All others</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>Expulsado</td>
+                  </tr>
+                  </tbody>
+                  <tfoot>
+                  <tr>
+                    <th>Rendering engine</th>
+                    <th>ssss</th>
+                    <th>Platform(s)</th>
+                    <th>Engine version</th>
+                    <th>CSS grade</th>
+                  </tr>
+                  </tfoot>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.Fin de contenido -->
+
+<!-- footer -->
+<?php include ('../recursos/footer.php') ?>
+<!-- fin de footer -->
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="../plugins/jszip/jszip.min.js"></script>
+<script src="../plugins/pdfmake/pdfmake.min.js"></script>
+<script src="../plugins/pdfmake/vfs_fonts.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../dist/js/demo.js"></script>
+<!-- Page specific script -->
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+</body>
+</html>
