0cd94e8477148d6367a4a292a23850e3bf4ff808
Stiven Castro Rios <riosstiven44@gamil.com>
2022-11-26 09:33:16 -0500

    Ajuste Header Tareas Usuario

--- modulos/panel.php
+++ modulos/panel.php
@@ -7,7 +7,15 @@ $conexion = $objeto->Conectar();
 $sentenciaSQL=$conexion->prepare("SELECT count(*) totalClientes FROM `clientes`");
 $sentenciaSQL->execute();
 $registro = $sentenciaSQL->fetch(PDO::FETCH_ASSOC);
-
 $totalClientes=$registro['totalClientes'];
+
+
+//$id_usr = ($_SESSION['usuario']['id']);
+//echo ("<script> alert ('$id_usr')</script> ");
+$sentenciaSQL=$conexion->prepare("SELECT count(*) totalTareas FROM `todo_list` WHERE id_usr='".$id_usr."' and estado_tarea = 'activa' ");
+$sentenciaSQL->execute();
+$registro = $sentenciaSQL->fetch(PDO::FETCH_ASSOC);
+
+$totalTareas=$registro['totalTareas'];
 
 ?>
--- recursos/header.php
+++ recursos/header.php
@@ -75,64 +75,37 @@
         </div>
       </li>
 
-      <!-- Messages Dropdown Menu -->
+       <!-- Consulta en php -->
+
+      <!-- Messages Dropdown Menu para tareas -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-comments"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
+          <i class="fas fa-tasks"></i>
+          <span class="badge badge-danger navbar-badge"><?php echo $totalTareas; ?></span>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <a href="#" class="dropdown-item">
+           
             <!-- Message Start -->
             <div class="media">
               <img src="<?php echo $url;?>/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
-                  Usuario Fienco 1
+                  Hola <?php echo $usuariof ?>
                   <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
                 </h3>
-                <p class="text-sm">Pruebas de Mensaje...</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                <p class="text-sm">Tienes <?php echo $totalTareas; ?> Tareas pendientes </p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> Fineco App</p>
               </div>
             </div>
             <!-- Message End -->
           </a>
+          
           <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="<?php echo $url;?>/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  John Pierce
-                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">I got your message bro</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="<?php echo $url;?>/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Nora Silvester
-                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">The subject goes here</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+          <a href="<?php echo $url;?>/F_inicio" class="dropdown-item dropdown-footer">Ver todas las tareas</a>
         </div>
       </li>
+
       <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
