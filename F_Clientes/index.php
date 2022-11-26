56dd60c151adb3c68fb86a95bcf47f5d4825a930
Stiven Castro Rios <riosstiven44@gamil.com>
2022-11-26 08:11:39 -0500

    Ajuste Jquery y Bootstrap Clientes

--- F_Clientes/index.php
+++ F_Clientes/index.php
@@ -273,17 +273,18 @@
 </div>
 
 
+<!-- jQuery -->
+<!-- <script src="../plugins/jquery/jquery.min.js"></script> -->
+<!-- Bootstrap 4 -->
+<!-- <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+<!-- DataTables  & Plugins -->
 
 <!-- footer -->
 <?php include ('../recursos/footer.php') ?>
 <!-- <script src="../dist/js/pages/dashboard.js"></script> -->
 <!-- fin de footer -->
 
-<!-- jQuery -->
-<script src="../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables  & Plugins -->
+
 <script src="../plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
 <script src="../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
@@ -293,15 +294,15 @@
 <script src="../plugins/jszip/jszip.min.js"></script>
 <script src="../plugins/pdfmake/pdfmake.min.js"></script>
 <script src="../plugins/pdfmake/vfs_fonts.js"></script>
-
 <script src="../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
 <script src="../plugins/datatables-buttons/js/buttons.print.min.js"></script>
 <script src="../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
 <!-- SweetAlert2 -->
 <script src="../plugins/sweetalert2/sweetalert2.all.js"></script>
 <!-- Alertas Toastr  -->
 <script src="../plugins/toastr/toastr.min.js"></script>
 
-
 <!-- scrip personalizados -->
-<script type="text/javascript" src="clientes.js"></script>  
+<script type="text/javascript" src="clientes.js"></script> 
+
