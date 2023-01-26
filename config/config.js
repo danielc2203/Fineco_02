d5fb6549cd8d4f3d4bb36c789aa1be618336a90d
Daniel Castro R <danielc2203@gmail.com>
2023-01-25 19:50:27 -0500

    recorte de codigo config.js

--- config/config.js
+++ config/config.js
@@ -38,7 +38,7 @@ function consultar() {
 					html += "<span class='text'>" + valores.nombre + "</span>";
 					html += '</div>'
 					html += '<div class="tools">' // Botones
-					html += '<a class="btn btn-outline-primary btnEditardd" id=' + valores.id + '><i class="fas fa-edit"></i>'
+					html += '<a class="btn btn-outline-primary btnEditar" id=' + valores.id + '><i class="fas fa-edit"></i>'
 					html += "<a class='btn btn-outline-danger btn-borrar-tarea' id='" + valores.id + "' type='button'><i class='fas fa-trash'></i></a>"
 					html += '</div>'
 					html += '</li>'
@@ -56,7 +56,7 @@ function consultar() {
 
 
 
-$(document).on('click', '.btnEditardd', function(){
+$(document).on('click', '.btnEditar', function(){
 	var id = $(this).attr('id');
 	var nombre = $(this).closest('li').find('.text').text();
 	var html = "";
@@ -132,127 +132,3 @@ $('#formModal').submit(function(e){
 	  });										     			
 });
 
-
-
-
-
-// editar IA
-$(document).on("click", ".btnEditar", function(){
-    opcion = 5; //Para llamar los datos de la BD
-    var id = $(this).attr('id');
-	alert(tabla);
-
-    //Aquí haces una llamada ajax para obtener los datos del usuario con el id correspondiente
-    $.ajax({
-        url: "crud.php",
-        method: "POST",
-        dataType: "json",
-        data: {opcion: 5, id:id, tabla:tabla},
-        success: function (response) {
-			response = JSON.parse(response);
-            var html = "";
-     
-           
-            //$('#modalConfig').modal('show');
-
-			html += '<div class="form-group">'
-			html += '<label for="id" class="col-form-label">ID</label>'
-			html += '<div class="col-sm">'
-			html += '<input type="text" class="form-control id" id="id" value="'+id+'" readonly>'
-			html += '</div>'
-			html += '</div>'
-
-			html += '<div class="form-group">'
-			html += '<label for="id" class="col-form-label">Nombre</label>'
-			html += '<div class="col-sm">'
-			html += '<input type="text" class="form-control nombre" id="nombre" value="'+nombre+'" >'
-			html += '</div>'
-			html += '</div>'
-
-
-			html += '<div class="modal-footer">'
-			html += '<button type="button" class="btn btn-success" id="guardar">Guardar</button>'
-			html += '<button type="button" class="btn btn-danger float-right" data-dismiss="modal">Cerrar</button>'
-			html += '</div>'
-
-
-			$(".modal-header").css("background-color", "#0267EB");
-			$(".modal-header").css( "color", "white" );
-			$(".modal-title").text(+nombre + "Formulario de edición de  " +tabla );
-			$('#modalConfig').modal('show');
-        }
-		
-    });
-	
-});
-
-
-
-//Editar
-$(document).on("click", ".btnEditarss", function(){
-		opcion = 5;//Para llamar los datos de la BD
-        var id = $(this).attr('id');
-		var nombre = nombre;
-        alert(id);
-		alert(nombre);
-
-		$.ajax({
-			//tabla: "convenios",
-			url: "crud.php",
-			method: "POST",
-			datatype:"json",    
-			data:  {opcion:opcion, tabla:tabla},    
-			success: function (response) {
-				
-				// Parse the json result
-				response = JSON.parse(response);
-				
-				var html = "";
-				
-				// Check if there is available records
-				if(response.length) {
-					// Loop the parsed JSON
-					$.each(response, function(key,value) {
-						// Our employee list template
-						var nombre = value.nombre;
-						//$("#nombre").val(nombre);
-						alert(nombre);
-					});
-				}
-				
-	html += '<div class="form-group">'
-    html += '<label for="id" class="col-form-label">ID</label>'
-    html += '<div class="col-sm">'
-    html += '<input type="text" class="form-control id" id="id" value="'+id+'" readonly>'
-    html += '</div>'
-    html += '</div>'
-
-    html += '<div class="form-group">'
-    html += '<label for="id" class="col-form-label">Nombre</label>'
-    html += '<div class="col-sm">'
-    html += '<input type="text" class="form-control id" id="nombre" value="">'
-    html += '</div>'
-    html += '</div>'
-
-
-    html += '<div class="modal-footer">'
-    html += '<button type="button" class="btn btn-success" id="guardar">Guardar</button>'
-    html += '<button type="button" class="btn btn-danger float-right" data-dismiss="modal">Cerrar</button>'
-	html += '</div>'
-
-
-    $("#contenido_config").html(html);
-
-    $(".modal-header").css("background-color", "#0267EB");
-    $(".modal-header").css( "color", "white" );
-    $(".modal-title").text("Calculadora de Creditos - Docentes");
-    $('#modalConfig').modal('show');
-				
-			}
-			
-		  });	
-
-
-		  
-    
-});
