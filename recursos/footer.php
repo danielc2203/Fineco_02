accebc5781a0bf3ede35ed1ba030ad3538d1a508
Daniel Castro R <danielc2203@gmail.com>
2023-06-24 15:32:59 -0500

    mensaje de demo OFF

--- recursos/footer.php
+++ recursos/footer.php
@@ -43,7 +43,7 @@
 <!-- AdminLTE App -->
 <script src="<?php echo $url;?>/dist/js/adminlte.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="<?php echo $url;?>/dist/js/demo.js"></script>
+<!-- <script src="../dist/js/demo.js"></script> -->
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <!-- <script src="../dist/js/pages/dashboard.js"></script> -->
 </body>
